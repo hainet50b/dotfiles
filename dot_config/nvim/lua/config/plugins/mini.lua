e0e10818ae254c81fecfcb841efc10e42061b323
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-30 01:35:26 +0900

    Add mini.map module to mini.nvim

--- dot_config/nvim/lua/config/plugins/mini.lua
+++ dot_config/nvim/lua/config/plugins/mini.lua
@@ -3,6 +3,20 @@ return {
     "nvim-mini/mini.nvim",
     enabled = true,
     version = false,
-    config = function() require('mini.statusline').setup() end,
+    config = function()
+      require('mini.statusline').setup()
+
+      require('mini.map').setup {
+        window = {
+          focusable = true,
+        },
+      }
+      vim.api.nvim_set_hl(0, "MiniMapNormal", { fg = "#555555" })
+      vim.api.nvim_set_hl(0, "MiniMapSymbolLine", { fg = "#ffffff" })
+      vim.api.nvim_set_hl(0, "MiniMapSymbolView", { fg = "#aaaaaa" })
+      vim.keymap.set("n", "<space>mm", function()
+        MiniMap.toggle()
+      end)
+    end,
   },
 }
