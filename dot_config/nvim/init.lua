6e8f6ec5d33e921b7a56aae0c6217a2419eec0df
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-30 11:50:01 +0900

    Add shortcuts for Quickfix navigation

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -17,6 +17,9 @@ vim.keymap.set("n", "<space><space>x", ":source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+vim.keymap.set("n", "<M-j>", vim.cmd.cnext)
+vim.keymap.set("n", "<M-k>", vim.cmd.cprevious)
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking text.",
   group = vim.api.nvim_create_augroup("pmacho-highlight-yank", { clear = true }),
