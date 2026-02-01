5b3c5eddb9a76373c8e7963904aa451ef8c46b43
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-02-02 04:06:38 +0900

    Add dotfiles-only mode to Telescope find files keymaps

--- dot_config/nvim/lua/config/plugins/telescope.lua
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -26,15 +26,28 @@ return {
       vim.keymap.set("n", "<space>fe", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.stdpath('config'),
+        }
+      end)
+      vim.keymap.set("n", "<space>fE", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.stdpath('config'),
           hidden = true,
         }
       end)
       vim.keymap.set("n", "<space>ff", function()
+        require('telescope.builtin').find_files()
+      end)
+      vim.keymap.set("n", "<space>fF", function()
         require('telescope.builtin').find_files {
           hidden = true,
         }
       end)
       vim.keymap.set("n", "<space>fh", function()
+        require('telescope.builtin').find_files {
+          cwd = vim.fn.expand('~'),
+        }
+      end)
+      vim.keymap.set("n", "<space>fH", function()
         require('telescope.builtin').find_files {
           cwd = vim.fn.expand('~'),
           hidden = true,
@@ -43,15 +56,18 @@ return {
       vim.keymap.set("n", "<space>fb", function()
         require('telescope.builtin').buffers()
       end)
-
-      vim.keymap.set("n", "<space>fH", function()
-        require('telescope.builtin').help_tags()
-      end)
       vim.keymap.set("n", "<space>fp", function()
         require('telescope.builtin').find_files {
           cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
           hidden = true,
         }
+      end)
+
+      vim.keymap.set("n", "<space>hh", function()
+        require('telescope.builtin').help_tags()
+      end)
+      vim.keymap.set("n", "<space>hk", function()
+        require('telescope.builtin').keymaps()
       end)
 
       require "config.telescope.multigrep".setup()
