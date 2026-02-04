2946ac4ffbc7d5bdd0322e1bc2441a52cfeb984b
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-02-04 19:12:23 +0900

    Add oil.nvim configurations

--- dot_config/nvim/lua/config/plugins/oil.lua
+++ dot_config/nvim/lua/config/plugins/oil.lua
@@ -9,39 +9,59 @@ return {
       columns = {
         'icon',
       },
+      keymaps = {
+        ["<Esc>"] = { "actions.close", mode = "n" },
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      float = {
+        max_width = 0.85,
+        max_height = 0.85,
+        border = "single",
+        preview_split = "right",
+      },
+      preview = {
+      },
     }
 
     local oil = require('oil')
 
     vim.keymap.set(
       "n", "<Space>ee", function()
-        oil.open_float(vim.fn.stdpath("config"))
+        oil.open_float(vim.fn.stdpath("config"), {
+          preview = {
+          },
+        })
       end,
       { desc = "Open Neovim config directory in explorer (oil.nvim)" }
     )
     vim.keymap.set(
       "n", "<Space>ed", function()
-        oil.open_float(vim.uv.cwd())
+        oil.open_float(vim.uv.cwd(), {
+          preview = {
+          },
+        })
       end,
       { desc = "Open current directory in explorer (oil.nvim)" }
     )
     vim.keymap.set(
       "n", "<Space>ef", function()
-        oil.open_float()
+        oil.open_float(vim.fn.expand("%:p:h"), {
+          preview = {
+          },
+        })
       end,
       { desc = "Open current file directory in explorer (oil.nvim)" }
     )
     vim.keymap.set(
       "n", "<Space>eh", function()
-        oil.open_float(vim.fn.expand("~"))
+        oil.open_float(vim.fn.expand("~"), {
+          preview = {
+          },
+        })
       end,
       { desc = "Open home directory in explorer (oil.nvim)" }
-    )
-    vim.keymap.set(
-      "n", "<Space>ec", function()
-        oil.close()
-      end,
-      { desc = "Close explorer (oil.nvim)" }
     )
   end,
 }
